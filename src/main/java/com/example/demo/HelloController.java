0ec098ae4a15d3cf858c1cec6c01a8e90b71821c
laoli1234 <ty1430748339@126.com>
2026-03-08 14:38:03 +0800

    新增推送到镜像仓库和k8s第二次尝试

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @GetMapping("/")
     public String sayHello() {
         // 返回"hello"，浏览器访问时就会显示这个内容
-        return "hello !!! this is spring boot by lty,修改2次代码，自动触发测试2";
+        return "hello !!! this is spring boot by lty,修改3次代码，自动触发测试3";
     }
 }
